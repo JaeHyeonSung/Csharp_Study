24e80dbc4a4aaafcd91f6477a6d936b490220675
JaehyeonSung <top91652@gmail.com>
2024-11-29 10:59:59 +0900

    Method Ends with Prac Test

--- Methods/obj/Debug/net8.0/Methods.AssemblyInfo.cs
+++ Methods/obj/Debug/net8.0/Methods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6db3edd6305e52df41839043674465ef224149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04f8781e345c1f744478457275fae4ba352764a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
